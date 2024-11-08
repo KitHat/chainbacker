b955bcd277809b597ecc7763630c9d7f03656cb4
lukianov <lukyanovvitaliymailbox@gmail.com>
2024-11-08 11:31:44 +0400

    build: init commit

---
+++ chainbacker-frontend/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="welcome-screen">
+    <h1>Welcome chain backer</h1>
+    <img class="welcome-screen__image" src="./assets/images.jpeg" alt="">
+  </div>
+</template>
+
+<style scoped>
+.welcome-screen {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+.welcome-screen__image {
+  width: 300px;
+  height: 300px;
+}
+</style>
