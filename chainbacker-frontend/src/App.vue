fbd50a217b34266c13e17d8740d19f6c0e184b69
lukianov <lukyanovvitaliymailbox@gmail.com>
2024-11-10 15:27:02 +0400

    fix: z index fix

--- chainbacker-frontend/src/App.vue
+++ chainbacker-frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div class="pb-[120px] pl-[28px] pr-[28px]">
     <router-view />
   </div>
-  <Tabbar @on-open="openCreateModal" class="max-w-[768px] fixed bottom-[20px]" />
+  <Tabbar @on-open="openCreateModal" class="max-w-[768px] fixed bottom-[20px] z-50" />
   <Teleport to="body">
     <Dialog v-model:visible="isCreateModalOpened" modal header="Create project" :style="{ width: '25rem' }">
       <CreateModal @on-close="isCreateModalOpened = false"
