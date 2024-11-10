d81c5f75b87a71ba9c1a4e10761f5901261cb04f
Nikita Khateev <nikita.khateev@openzeppelin.com>
2024-11-10 17:00:25 +0400

    add backing to smart contract

--- contract/wrappers/Reputation.ts
+++ contract/wrappers/Reputation.ts
@@ -39,14 +39,26 @@ export class Reputation implements Contract {
     async sendNewKick(provider: ContractProvider, via: Sender, value: bigint, queryId: bigint, target: bigint, kickEnd: bigint, title: string, timestamp: number, levels: [bigint, bigint][]) {
         let toHash = `${timestamp}${title}`;
         let marker = crypto.createHash('md5').update(toHash).digest().readBigUint64BE();
-        let cell = beginCell().storeUint(2, 32).storeUint(queryId, 64).storeUint(target, 256).storeUint(marker, 256).storeUint(kickEnd, 64).storeUint(levels.length, 16);
-        for (const level of levels) {
-            cell = cell.storeUint(level[0], 256).storeUint(level[1], 16);
+        let levelsReversed = levels.reverse();
+        let lastRef = null;
+        for (const level of levelsReversed) {
+            let next = beginCell();
+            if (lastRef) {
+                next.storeRef(lastRef);
+            }
+            lastRef = next.storeUint(level[0], 128).storeUint(level[1], 16).endCell();
         }
+
+        if (!lastRef) {
+            throw ("there should be levels");
+        }
+
+        let cell = beginCell().storeUint(2, 32).storeUint(queryId, 64).storeUint(target, 128).storeUint(marker, 256).storeUint(kickEnd, 64).storeRef(lastRef).endCell();
+
         await provider.internal(via, {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: cell.endCell()
+            body: cell
         })
     }
 
