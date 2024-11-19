7700b7b019a9e3b8ed50e263ec067d59c310175d
Nikita Khateev <nikita.khateev@openzeppelin.com>
2024-11-19 17:25:28 +0300

    more changs

---
+++ contract/wrappers/JettonWallet.compile.ts
@@ -0,0 +1,5 @@
+import { CompilerConfig } from '@ton/blueprint';
+
+export const compile: CompilerConfig = {
+    targets: ['tests/utils/contracts/params.fc', 'tests/utils/contracts/op-codes.fc', 'tests/utils/contracts/jetton-utils.fc', 'tests/utils/contracts/jetton-wallet.fc'],
+};
