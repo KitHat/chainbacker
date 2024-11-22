5b3b4c4509489d3faf0a934f2b3b305cad65c4be
Nikita Khateev <nikita.khateev@openzeppelin.com>
2024-11-22 11:24:08 +0300

    update script

--- contract/scripts/deployKick.ts
+++ contract/scripts/deployKick.ts
@@ -1,6 +1,7 @@
-import { toNano } from '@ton/core';
+import { Address, toNano } from '@ton/core';
 import { Kick } from '../wrappers/Kick';
 import { compile, NetworkProvider } from '@ton/blueprint';
+import { JettonMinter } from '../wrappers/JettonMinter';
 
 export async function run(provider: NetworkProvider) {
     let addr = provider.sender().address;
@@ -8,11 +9,20 @@ export async function run(provider: NetworkProvider) {
         throw (123);
     }
     let code = await compile("Backer");
-    const reputation = provider.open(Kick.createFromConfig({ target: 100000000n, expiration: BigInt(Date.now() + 60 * 60 * 24 * 3), creator: addr, milestones: [{ part: 100n }], tiers: [{ amount: 1000n, price: 100000n }], code }, await compile('Kick')));
+    const kick = provider.open(Kick.createFromConfig({ target: 100000000n, expiration: BigInt(Date.now() + 60 * 60 * 24 * 3), creator: addr, milestones: [{ part: 100n }], tiers: [{ amount: 1000n, price: 100000n }], code }, await compile('Kick')));
 
-    await reputation.sendDeploy(provider.sender(), toNano('0.1'));
+    await kick.sendDeploy(provider.sender(), toNano('0.1'));
 
-    await provider.waitForDeploy(reputation.address);
+    await provider.waitForDeploy(kick.address);
+
+    const kickAddress = kick.address;
+    const reputation = provider.open(Kick.createFromAddress(kickAddress));
+
+    const usdtMaster = provider.open(JettonMinter.createFromAddress(Address.parse("kQD0GKBM8ZbryVk2aESmzfU6b9b_8era_IkvBSELujFZPsyy")));
+
+    const kickUsdtAddr = await usdtMaster.getWalletAddress(kickAddress);
+
+    let res = await reputation.sendUsdtWallet(provider.sender(), toNano('0.05'), 2n, kickUsdtAddr);
 
     // TODO: send method
 }
